7d21d1c9a81d651ec19b63f600398d7e9527bafb
Jingyi12333 <55928513+Jingyi12333@users.noreply.github.com>
2026-03-10 12:40:16 -0400

    Add files via upload

---
+++ project 1/app.js
@@ -0,0 +1,66 @@
+const { createApp } = Vue;
+const { createVuetify } = Vuetify;
+const vuetify = createVuetify();
+
+
+const App = {
+    setup() {
+        const collection = [
+
+            {
+                name: "Jasmine Tea",
+                taste: "tastes like green tea because people usually mix it with green tea",
+                smell: "literally best smell ever very floral and fragrant",
+                bio: "my life peaked when i saw the little flower bloom in my cup",
+                image: "images/jasmine.png"
+            },
+
+            {
+                name: "Osmanthus Tea",
+                taste: "just like normal oolong or black or green or red tea because that's what it's usually mixed with",
+                smell: "amazing smell like honey very fragrant",
+                bio: "insert genshin impact meme",
+                image: "images/osmanthus.png"
+            },
+
+            {
+                name: "silver needle white tea",
+                taste: "very mild",
+                smell: "mild smell",
+                bio: "tea with a cool origin story of being high up in a mountain. there's white spots all over that looks like mold but apparently its the amino acids leaking out",
+                image: "images/silver needle.png"
+            },
+
+            {
+                name: "buckwheat tea",
+                taste: "like buckwheat, so warm bread",
+                smell: "like rice",
+                bio: "tea for people who wants to drink flavoured water at 10pm ",
+                image: "images/buckwheat.png"
+            },
+
+            {
+                name: "pu er tea",
+                taste: "bitter",
+                smell: "pungent like it's punching you",
+                bio: "tea if we made tea like it's cheese. it gets fermented and pressed into a wheel",
+                image: "images/pu er.png"
+            },
+
+            {
+                name: "guava tea",
+                taste: "mild guava taste",
+                smell: "mild guava smell",
+                bio: "another flavoured water tea. apparently it lowers blood sugar?",
+                image: "images/guava.png"
+            }
+        ]
+
+        return {
+            collection
+
+        }
+    }
+}
+createApp(App).use(vuetify).mount('#app');
+
